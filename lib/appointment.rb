3e0ebc78a6273b4cb512a85a4692f958688d212f
Tracy Ngo <tngo1206@gmail.com>
2019-03-23 21:38:24 +0000

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,18 @@
+class Appointment
+
+attr_accessor  :patient, :doctor, :date
+
+@@all = []
+
+  def initialize (patient, doctor, date)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+
+end
